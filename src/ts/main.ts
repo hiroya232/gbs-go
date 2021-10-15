88662a249feda842e7714510d5a81394ecef7251
hiroya-akeoka <mowoarm232@gmail.com>
2021-10-15 20:26:54 +0900

    :shirt:フォーマッタ適用

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,14 +1,16 @@
 window.onload = () => {
-  const getMultiListButton = document.querySelector('[data-trigger="getMultiListButton"]');
+    const getMultiListButton = document.querySelector(
+        '[data-trigger="getMultiListButton"]'
+    );
 
-  getMultiListButton.addEventListener('click', function () {
-    let loc = window.location;
-    let uri = "ws://" + loc.host + "/stream";
+    getMultiListButton.addEventListener('click', function () {
+        let loc = window.location;
+        let uri = 'ws://' + loc.host + '/stream';
 
-    const ws = new WebSocket(uri);
-    ws.onmessage = function (evt) {
-      let out = document.getElementById("multiList");
-      out.innerHTML += evt.data + "<br>";
-    };
-  })
-}
+        const ws = new WebSocket(uri);
+        ws.onmessage = function (evt) {
+            let out = document.getElementById('multiList');
+            out.innerHTML += evt.data + '<br>';
+        };
+    });
+};
