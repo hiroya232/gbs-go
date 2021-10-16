9c37be26dc9241a9dde280f20e9a25dbbec7e335
hiroya-akeoka <mowoarm232@gmail.com>
2021-10-16 21:53:28 +0900

    :recycle:救援ツイート取得機能をパッケージとして切り出し

--- main.go
+++ main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
-	"database/sql"
-
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
-	"golang.org/x/net/websocket"
+	"github.com/hiroya232/gbs/multi"
 )
 
 func main() {
 	dir, _ := os.Getwd()
 	http.HandleFunc("/", multiHandler)
-	http.HandleFunc("/stream", getMultiList)
+	http.HandleFunc("/stream", multi.GetMultiList)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir(dir+"/public/"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -29,74 +20,4 @@ func main() {
 func multiHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("src/html/multiList.html")
 	t.Execute(w, nil)
-}
-
-type MultiInfo struct {
-	MultiId string `json:"multi_id"`
-	Enemy   string `json:"enemy"`
-}
-
-func newMultiInfo(multiId string, enemy string) *MultiInfo {
-	multiInfo := new(MultiInfo)
-	multiInfo.MultiId = multiId
-	multiInfo.Enemy = enemy
-	return multiInfo
-}
-
-func getMultiList(w http.ResponseWriter, r *http.Request) {
-
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	dbconf := "root:@tcp(127.0.0.1:3306)/gbs_go?charset=utf8mb4"
-	db, err := sql.Open("mysql", dbconf)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	defer db.Close()
-
-	config := oauth1.NewConfig(os.Getenv("TWITTER_COMSUMER_KEY"), os.Getenv("TWITTER_COMSUMER_SECRET_KEY"))
-	token := oauth1.NewToken(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
-	httpClient := config.Client(oauth1.NoContext, token)
-	client := twitter.NewClient(httpClient)
-
-	params := &twitter.StreamFilterParams{
-		Track:         []string{"参加者募集！"},
-		StallWarnings: twitter.Bool(true),
-	}
-	stream, err := client.Streams.Filter(params)
-	if err != nil {
-		fmt.Println("ツイートの取得に失敗しました。")
-	}
-
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
-		for {
-			ch := <-stream.Messages
-			switch tweet := ch.(type) {
-			case *twitter.Tweet:
-				fmt.Println("\n------------------------------------------------------------------------\n ")
-
-				splitTweet := strings.Split(tweet.Text, "\n")
-				splitMultiId := strings.Split(splitTweet[0], " ")
-
-				multiInfo := newMultiInfo(splitMultiId[len(splitMultiId)-2], splitTweet[2])
-				fmt.Println(multiInfo)
-				multiInfoJson, err := json.Marshal(multiInfo)
-				if err != nil {
-					log.Fatalln(err)
-				}
-				fmt.Println(string(multiInfoJson))
-
-				err = websocket.Message.Send(ws, string(multiInfoJson))
-				if err != nil {
-					log.Fatalln(err)
-				}
-			default:
-				break
-			}
-		}
-	}).ServeHTTP(w, r)
 }
---
+++ multi/get_multi.go
@@ -0,0 +1,86 @@
+package multi
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+	"golang.org/x/net/websocket"
+)
+
+type MultiInfo struct {
+	MultiId string `json:"multi_id"`
+	Enemy   string `json:"enemy"`
+}
+
+func newMultiInfo(multiId string, enemy string) *MultiInfo {
+	multiInfo := new(MultiInfo)
+	multiInfo.MultiId = multiId
+	multiInfo.Enemy = enemy
+	return multiInfo
+}
+
+func GetMultiList(w http.ResponseWriter, r *http.Request) {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	dbconf := "root:@tcp(127.0.0.1:3306)/gbs_go?charset=utf8mb4"
+	db, err := sql.Open("mysql", dbconf)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer db.Close()
+
+	config := oauth1.NewConfig(os.Getenv("TWITTER_COMSUMER_KEY"), os.Getenv("TWITTER_COMSUMER_SECRET_KEY"))
+	token := oauth1.NewToken(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"参加者募集！"},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := client.Streams.Filter(params)
+	if err != nil {
+		fmt.Println("ツイートの取得に失敗しました。")
+	}
+
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		for {
+			ch := <-stream.Messages
+			switch tweet := ch.(type) {
+			case *twitter.Tweet:
+				fmt.Println("\n------------------------------------------------------------------------\n ")
+
+				splitTweet := strings.Split(tweet.Text, "\n")
+				splitMultiId := strings.Split(splitTweet[0], " ")
+
+				multiInfo := newMultiInfo(splitMultiId[len(splitMultiId)-2], splitTweet[2])
+				fmt.Println(multiInfo)
+				multiInfoJson, err := json.Marshal(multiInfo)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				fmt.Println(string(multiInfoJson))
+
+				err = websocket.Message.Send(ws, string(multiInfoJson))
+				if err != nil {
+					log.Fatalln(err)
+				}
+			default:
+				break
+			}
+		}
+	}).ServeHTTP(w, r)
+}
