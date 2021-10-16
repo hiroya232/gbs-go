1e0f7aa3d71fceee87bb2d87ef69b19fcb1a7310
hiroya-akeoka <mowoarm232@gmail.com>
2021-10-16 09:21:03 +0900

    :shirt:lint修正

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,12 +7,12 @@ window.onload = () => {
     );
 
     getMultiListButton.addEventListener('click', () => {
-        let loc = window.location;
-        let uri = 'ws://' + loc.host + '/stream';
+        const loc = window.location;
+        const uri = 'ws://' + loc.host + '/stream';
 
         const ws = new WebSocket(uri);
         ws.onmessage = function (evt) {
-            let out = document.getElementById('multiList');
+            const out = document.getElementById('multiList');
             out.innerHTML += evt.data + '<br>';
         };
         stopGetMultiListButton.addEventListener('click', () => {
